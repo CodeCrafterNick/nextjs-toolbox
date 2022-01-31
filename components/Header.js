6b74af067d7845de09ecb171e7445bbd2744cbd3
Prince Wilson <maxcell.wilson@gmail.com>
2022-01-31 16:06:37 -0500

    Initial commit

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,3 @@
 export default function Header({ title }) {
-  return (
-    <>
-      <h1 className="title">{title}</h1>
-      <p>
-        Hi 👋! This template gives you a{" "}
-        <a href="@components/Header">Next.js</a> app with the scaffolding for{" "}
-        <a href="@components/Header">
-          Netlify Functions
-        </a>
-        , <a href="@components/Header">Forms</a>, and{" "}
-        <a href="@components/Header">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
-      </p>
-
-      <p>
-        You can find the code for this project on GitHub at{" "}
-        <a href="https://github.com/netlify-templates/nextjs-toolbox">
-          https://github.com/netlify-templates/nextjs-toolbox
-        </a>
-        ! Happy coding!
-      </p>
-    </>
-  );
+  return <h1 className="title">{title}</h1>
 }
