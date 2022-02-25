49f565f6e0e2c86e26d664c41ecfae9018138501
Prince Wilson <maxcell.wilson@gmail.com>
2022-02-24 22:02:49 -0500

    fix: Rename ContactForm -> FeedbackForm

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,30 +1,31 @@
-import styles from './FeedbackForm.module.css'
+import styles from './ContactForm.module.css'
 
-export default function FeedbackForm() {
+export default function ContactForm() {
   return (
       <form
         className={styles.form}
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
+        netlify
+        netlify-honeypot
         name="feedback"
         method="POST"
-        action="/pages/success"
+        action="/thanks"
       >
         <input type="hidden" name="form-name" value="feedback" />
+
         <p className={styles.hidden}>
             <label>
             Don’t fill this out if you’re human: <input name="bot-field" />
             </label>
         </p>
   
-        <label htmlFor="name">Name</label>
-        <input id="name" className={styles['form-field']} type="text" name="name" />
+        <label for="name">Name</label>
+        <input className={styles['form-field']} type="text" name="name" />
 
-        <label htmlFor="email">Email</label>
-        <input id="email" className={styles['form-field']} type="email" name="email" required />
+        <label for="name">Email</label>
+        <input className={styles['form-field']} type="email" name="email" required />
 
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
+        <label for="feedback">What is your feedback?</label>
+        <textarea className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
         <button className={styles.button} type="submit">Submit</button>
       </form>
   )
