3efa3672dc09ae5d8691bc7ae56f087949754804
Prince Wilson <maxcell.wilson@gmail.com>
2022-02-28 15:56:52 -0500

    fix (#18): Adds JokeBlock to index page (#19)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import ContactForm from '@components/FeedbackForm'
+import FeedbackForm from '@components/FeedbackForm'
+import JokeBlock from '@components/JokeBlock'
 
 export default function Home() {
   return (
@@ -16,9 +17,9 @@ export default function Home() {
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the submissions can be found in the Netlify Admin site.
         </p>
-        <ContactForm />
+        <FeedbackForm />
+        <JokeBlock/>
       </main>
-
       <Footer />
     </div>
   )
