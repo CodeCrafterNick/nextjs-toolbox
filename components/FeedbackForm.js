3a5a8e3538e34dbf38582bb3d15d0eacae50f368
Nick <xnasax@gmail.com>
2022-06-15 14:36:49 -0400

    Initial

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -8,7 +8,7 @@ export default function ContactForm() {
         netlify-honeypot
         name="feedback"
         method="POST"
-        action="/success"
+        action="/pages/success"
       >
         <input type="hidden" name="form-name" value="feedback" />
 
